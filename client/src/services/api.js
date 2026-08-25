37f81ea27c4154b3e7a4ea6f3e23ddc95bb42c18
Luckx <luquinhasamuel913@gmail.com>
2026-08-25 15:55:19 -0300

    Connect frontend to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('nuvvo_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
